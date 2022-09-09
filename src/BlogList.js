831fd1a4c7f6733af034cc2612c0f38c995a0ab9
Sumit Kumar Jha <88476958+sumit-kumar-jha1709@users.noreply.github.com>
2022-09-09 22:48:37 +0530

    Add files via upload

---
+++ src/BlogList.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const BlogList = ({blogs, title,}) => {
+
+  return (
+    <div className="Blog-List">
+      <h1>{title}</h1>
+        {blogs.map((blog) => (
+          <div className="blog-preview" key={blog.id}>
+            <Link to={`/blogs/${blog.id}`}>
+             <h2>{blog.title}</h2>
+             <p>Written By {blog.author}</p>
+            </Link>
+                        
+          </div>
+       ))} 
+    </div>
+  )
+}
+
+export default BlogList
---
+++ src/Navbar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <Link to="/"><h1>Da-Blog</h1></Link>
+      <div className="links">
+        <Link to="/">Home</Link>
+        <Link to="/create">New Blog</Link>
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar
